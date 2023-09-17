36e2a12599ffdbb62b19bbfce6d17d610bcf098e
asaddevvv <nabievasad17@gmail.com>
2023-09-17 19:40:54 +0000

    js

---
+++ script.js
@@ -0,0 +1,72 @@
+// console.log("FR-52");
+//!let, const, 
+// let n1= "hello world"
+// console.log(n1);
+
+// let str = "hello world"    
+// console.log(str);
+
+
+// let str = "front";
+// console.log(typeof str);
+
+// let numb = 4;
+// console.log( typeof numb);
+
+// let undef = undefined ;
+// console.log( typeof undef);
+
+// let nul = null;
+// console.log(typeof nul);
+
+// let bool = false; 
+// console.log(typeof bool);
+
+// let obj = {};
+// console.log(typeof obj);
+
+// let bigInteger = 87654654356546765n;
+// console.log(typeof bigInteger);
+
+// let symb = Symbol("bhghj"); 
+// console.log(typeof symb);
+
+// const first = 10
+// const second = 2
+
+// console.log(first + second);
+// console.log(first - second);
+// console.log(first * second);
+// console.log(first / second);
+
+// !домашнее задание
+
+// let num = 2
+// let str = '3'
+// console.log(num + str);
+
+// let num = 55
+// console.log(typeof  num);
+
+//  var str = "hello world"
+//  console.log(str);
+
+// let gtt = null;
+//  console.log(typeof gtt);
+
+// let bool = false;
+// console.log(typeof bool);
+
+// let obj = {};
+// console.log(typeof obj);
+
+// let bigint = 1432434343434n;
+// console.log(typeof bigint);
+
+// let sib = Symbol ("asad");
+// console.log(typeof sib);
+
+
+
+
+
